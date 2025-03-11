d3fa1cb82ba4ca0056f03e10a555bf41a860a2d3
Chris Bilger <christopherbilg@gmail.com>
2025-03-11 09:37:30 -0400

    Update initialize-reveal-js-and-highlight-js.js

--- presentations/js/initialize-reveal-js-and-highlight-js.js
+++ presentations/js/initialize-reveal-js-and-highlight-js.js
@@ -1,9 +1,11 @@
 import Reveal from "../lib/reveal.js-5.1.0/reveal.esm.js";
 
+// Initialize the Reveal.js slide deck
 let deck = new Reveal({
   slideNumber: "c/t",
   help: true,
 });
 deck.initialize();
 
+// Highlight all highlight-able code locations on the webpage
 hljs.highlightAll();
