853bb830370c0f8e41afe3c615a0f65794ff952a
Chris Bilger <christopherbilg@gmail.com>
2026-01-08 18:41:41 +0000

    Add new image and enhance presentation content
    - Added a new image for the Turing machine to the presentation.
    - Expanded the "Problem Statement" section with detailed challenges in the assessment lifecycle.
    - Introduced new sections outlining the complexities and need for a structured approach in managing state transitions.
    - Improved the "State Machine Overview" and "Implementation Details" sections with additional content and images.
    - Updated the JavaScript initialization file to include new plugins for enhanced functionality in the presentation.

--- presentations/js/initialize-reveal-js-and-highlight-js.js
+++ presentations/js/initialize-reveal-js-and-highlight-js.js
@@ -1,9 +1,13 @@
+import Notes from "../lib/reveal.js-5.1.0/plugin/notes/notes.esm.js";
+import Search from "../lib/reveal.js-5.1.0/plugin/search/search.esm.js";
+import Zoom from "../lib/reveal.js-5.1.0/plugin/zoom/zoom.esm.js";
 import Reveal from "../lib/reveal.js-5.1.0/reveal.esm.js";
 
 // Initialize the Reveal.js slide deck
 let deck = new Reveal({
-  slideNumber: "c/t",
   help: true,
+  plugins: [Notes, Search, Zoom],
+  slideNumber: "c/t",
 });
 deck.initialize();
 
